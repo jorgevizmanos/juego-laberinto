0597a189f6aff2df703f95b9877ecc6e26318f82
eduar <eduardomarinperez2005@gmail.com>
2024-11-15 15:48:47 +0100

    puse un comentario de prueba

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+// hola que tal mi gente deyoutube etsamos aqui
 
     }
 }
